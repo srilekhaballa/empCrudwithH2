cb8fe8db24e21a363855e7bce8867a0f60fd8641
srilekhaballa <80448422+srilekhaballa@users.noreply.github.com>
2021-07-07 11:46:53 +0530

    del

--- basic.employee-crud/src/main/java/com/example/basic/employeecrud/dao/EmployeeDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.basic.employeecrud.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeDao extends JpaRepository<Employee , Long> {
-
-}
